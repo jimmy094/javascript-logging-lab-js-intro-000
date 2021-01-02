0f6e8be73a61c9d766c59303a5f7fbcf81387308
inherited-validator-1094 <jjim111@aol.com>
2021-01-02 22:45:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ console.error("HALP!")
 console.log("Something's broken")
 console.warn("Account possibly compromised!")
 
-var word
-word= "bird"
-console.log(word)
-word='dog'
-console.log(word)
+var word = 'bird'
